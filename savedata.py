a5a850ac435bfb89a473c495e4e075ae10804b0d
Igors Anastasjevs <ianastasjev1@gmail.com>
2024-05-29 11:17:23 +0300

    populated data2.xlsx and data3.xlsx

--- savedata.py
+++ savedata.py
@@ -3,10 +3,16 @@ import WLalg
 import time
 import networkx as nx
 import WLalgShevarshidze as WLV2
-def savedata(filename, Graphgenerator, graphisomorthismfunction):
+def generategraphs(graphgenerator):
+    graphs = dict()
+    for i in range(30, 1510, 10 ):
+        graph = graphgenerator(i)
+        graphs.update({i:graph})
+    return graphs
+def savedata(filename, graphs:dict, graphisomorthismfunction):
     data = dict()
-    for size in range(30, 1510, 10):
-        G = Graphgenerator(size)
+    for size in graphs.keys():
+        G = graphs[size]
         H = G.copy()
         data.update({size: []})
         for i in range(20):
@@ -26,8 +32,7 @@ def savedata(filename, Graphgenerator, graphisomorthismfunction):
             ws.cell(row=i, column=j + 2, value=data[key][j])
         i += 1
     wb.save(filename=filename)
-savedata('data2.xlsx', nx.dorogovtsev_goltsev_mendes_graph, WLalg.wlalg)
-savedata('data3.xlsx', nx.path_graph, WLV2.wlalgV2)
-savedata('data4.xlsx', nx.dorogovtsev_goltsev_mendes_graph, WLV2.wlalgV2)
-savedata('data5.xlsx',nx.path_graph, nx.is_isomorphic)
-savedata('data6.xlsx',nx.dorogovtsev_goltsev_mendes_graph,nx.is_isomorphic)
+graphs = generategraphs(nx.path_graph)
+
+savedata('data2.xlsx', graphs, WLV2.wlalgV2)
+savedata('data3.xlsx',graphs, nx.is_isomorphic)
